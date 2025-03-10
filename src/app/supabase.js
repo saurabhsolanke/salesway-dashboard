3193f5d3381fca68b841980882e0c4bc8a5ba039
Saurabh Solanke <35625425+saurabhsolanke@users.noreply.github.com>
2025-03-10 17:52:07 +0530

    SS change for nexrconfig & supabase

--- src/app/supabase.js
+++ src/app/supabase.js
@@ -1,6 +1,10 @@
- import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Missing Supabase URL or Anon Key');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
